44adf4957307185fed86fdc609e60c76f04145aa
Dmitry <ImRedBelly@gmail.com>
2021-03-16 19:30:08 +0300

    revers

---
+++ Assets/Player/Scripts/ReversLook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ReversLook : MonoBehaviour
+{
+    public GameObject reversObject;
+    Vector2 mousePosition;
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mousePosition.x > PlayerMovement.instance.transform.position.x)
+            reversObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+        else
+            reversObject.transform.localScale = new Vector3(-0.2f, 0.2f, 0.2f);
+    }
+}
