5eee3dcfa1722b8eaba79b21bfcce49d217d42de
JukovYuri <jukov_yuri@mail.ru>
2021-03-19 21:08:59 +0300

    111

--- Assets/Player/Scripts/Aim.cs
+++ Assets/Player/Scripts/Aim.cs
@@ -7,8 +7,46 @@ public class Aim : MonoBehaviour
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
+
+    public bool isUseHardwareCursor;
+
+    public float distanceFromPlayer = 2f;
+
+    public GameObject prefabCursor;
+    GameObject cursor;
+    SpriteRenderer sr;
+
+
     void Awake()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        if (isUseHardwareCursor)
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+            return;
+        }
+        Cursor.visible = false;
+    }
+
+    private void Start()
+    {
+        cursor = Instantiate(prefabCursor);
+    }
+
+    private void Update()
+    {
+        if (isUseHardwareCursor)
+        {
+            return;
+        }
+        ArroundPlayer();
+    }
+
+    void ArroundPlayer()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        direction.z = 0;
+
+        cursor.transform.position = transform.position + direction.normalized * distanceFromPlayer;
     }
 }
