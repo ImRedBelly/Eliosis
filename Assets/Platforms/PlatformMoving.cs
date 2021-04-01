74b0fc8f34450198224e831ae7346700e31e8bf9
JukovYuri <jukov_yuri@mail.ru>
2021-04-01 03:27:13 +0300

    111

---
+++ Assets/Platforms/PlatformMoving.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour
+{
+    public Transform[] points;
+    private Vector3[] waypoints;
+    public float speed;
+
+    private int currentWaypoint;
+    Rigidbody2D rb;
+
+    bool isCountDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        waypoints = new Vector3[points.Length];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            waypoints[i] = points[i].position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (waypoints == null || waypoints.Length <= 1)
+        {
+            return;
+        }
+
+
+
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint]) < 0.1f)
+        {
+
+            if (isCountDown)
+            {
+                currentWaypoint--;
+            }
+            else
+            {
+                currentWaypoint++;
+            }
+
+
+            if (currentWaypoint > waypoints.Length - 1)
+            {
+                currentWaypoint--;
+                isCountDown = true;
+            }
+            else if (currentWaypoint == 0)
+            {
+                isCountDown = false;
+            }
+
+        }
+
+        print(isCountDown + " / " + currentWaypoint);
+
+
+
+        Vector3 direction = waypoints[currentWaypoint] - transform.position;
+        direction.z = 0;
+        rb.velocity = direction.normalized * speed;
+
+    }
+}
